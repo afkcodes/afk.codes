47e4b7c29a36f3e8f8837b0b267baea64c3900ca
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-09 19:00:43 +0530

    changes for cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import react from '@vitejs/plugin-react'
-import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import vike from 'vike/plugin';
+import { UserConfig } from 'vite';
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
-}
+  plugins: [react(), vike()],
+  resolve: {
+    // Only needed for this example
+    // TODO: check if still needed
+    preserveSymlinks: true
+  }
+};
 
-export default config
+export default config;
