a4218d0939927cdf1798addcb0fa02c0a09ded21
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-09 18:12:25 +0530

    add projects section

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,8 +1,39 @@
-const Project = () => {
+import { ArrowRight } from 'lucide-react';
+import { ReactElement } from 'react';
+import { Link } from '../renderer/Link';
+import TextLight from './TextLight';
+
+interface Project {
+  name: string;
+  description: string;
+  year: number;
+  icon: ReactElement;
+  url?: string;
+}
+
+const Project: React.FC<Project> = ({ name, description, year, url, icon }) => {
   return (
-    <div className='bg-darkTertiary h-60 w-60 rounded-md p-4'>
-      <div>
-        <h4>Calender</h4>
+    <div className='flex justify-center hover:bg-darkTertiary h-56 w-56 rounded-md p-6 transition-all ease-in duration-300 border cursor-pointer'>
+      <div className='flex flex-col justify-center items-start gap-2'>
+        <div>{icon}</div>
+        <p>
+          <TextLight customClass='text-lg font-medium'>{name}</TextLight>
+        </p>
+        <p className='text-sm font-semibold'>{description}</p>
+
+        <Link
+          href={url}
+          className='underline underline-offset-2 hover:text-accentText transition-all duration-300'
+          target='_blank'
+        >
+          {url ? (
+            <div className='flex gap-1 items-center'>
+              Check on Github <ArrowRight size={20} />
+            </div>
+          ) : (
+            <p>Coming Soon</p>
+          )}
+        </Link>
       </div>
     </div>
   );
--- containers/Projects.tsx
+++ containers/Projects.tsx
@@ -1,3 +1,4 @@
+import { BookText, Calendar, Headphones, Music, Video, Zap } from 'lucide-react';
 import Project from '../components/Project';
 import TextLight from '../components/TextLight';
 
@@ -9,8 +10,47 @@ const Projects = () => {
           <TextLight customClass='font-bold text-3xl'>Projects</TextLight>
         </h2>
       </div>
-      <div>
-        <Project />
+      <div className='flex flex-wrap gap-12'>
+        <Project
+          name='Calendar'
+          description='A Clean Calendar component built with React and TailwindCSS.'
+          year={2023}
+          url='https://github.com/afkcodes/calendar'
+          icon={<Calendar size={22} />}
+        />
+        <Project
+          name='Audio_X'
+          description='A JavaScript Audio library that plays most popular formats.'
+          year={2023}
+          url='https://github.com/afkcodes/audio_x'
+          icon={<Headphones size={22} />}
+        />
+        <Project
+          name='Flutter Phosphor Icons'
+          description='A Icon library on top of Phosphor Icons for flutter.'
+          year={2023}
+          url='https://github.com/afkcodes/flutter_phosphor_icons'
+          icon={<Zap size={22} />}
+        />
+        <Project
+          name='Free Dictionary API'
+          description='A free dictionary api for the web.'
+          year={2023}
+          url='https://github.com/afkcodes/free-dictionary-api'
+          icon={<BookText size={22} />}
+        />
+        <Project
+          name='Sunoh'
+          description='A music and podcast player built on React Native.'
+          year={2023}
+          icon={<Music size={22} />}
+        />
+        <Project
+          name='Katha'
+          description='An app for watching Indian Mythological Shows and Videos.'
+          year={2023}
+          icon={<Video size={22} />}
+        />
       </div>
     </section>
   );
