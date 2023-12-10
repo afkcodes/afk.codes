8e6373b2899f0a28877eaa3e7921c9f4e5b80328
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-11 00:31:19 +0530

    update logo text bg style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import { LuTerminal } from 'react-icons/lu';
 import { Link } from '../renderer/Link';
 import Button from './Button';
 
@@ -7,8 +6,10 @@ const Header = () => {
     <header className='sticky top-0 z-10 bg-darkSecondary/40 backdrop-blur-2xl shadow-md py-3 '>
       <nav className='flex w-full justify-between items-center px-12 '>
         <Link href='/' className='flex justify-center items-center gap-2'>
-          <LuTerminal size={22} color='white' />
-          <p className='text-xl font-bold italic cursor-pointer'>afk.codes</p>
+          {/* <LuTerminal size={22} color='white' /> */}
+          <p className='text-xl font-bold italic cursor-pointer px-2 py-1 bg-hoverPrimaryButton'>
+            afk.codes
+          </p>
         </Link>
         <ul className='flex justify-center items-center gap-6'>
           <li>
