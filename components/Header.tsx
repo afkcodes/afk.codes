fcd91865302873934ecf8a2b31803dcffb43b233
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-11 21:35:17 +0530

    added smooth scrolling and work around for gap after navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,29 +3,28 @@ import Button from './Button';
 
 const Header = () => {
   return (
-    <header className='sticky top-0 z-10 bg-darkSecondary/40 backdrop-blur-2xl shadow-md py-3 '>
+    <header className='sticky top-[-0.1px] m-0 z-10 bg-darkSecondary/40 backdrop-blur-2xl shadow-md py-3 '>
       <nav className='flex w-full justify-between items-center px-12 '>
         <Link href='/' className='flex justify-center items-center gap-2'>
-          {/* <LuTerminal size={22} color='white' /> */}
           <p className='text-xl font-bold italic cursor-pointer px-2 py-1 bg-hoverPrimaryButton'>
             afk.codes
           </p>
         </Link>
         <ul className='flex justify-center items-center gap-6'>
           <li>
-            <Button text='About' type='ghost' radius='md' href='#intro' behavior='link' />
+            <Button text='About' type='ghost' radius='md' href='/#intro' behavior='link' />
           </li>
           <li>
-            <Button text='Projects' type='ghost' radius='md' href='#projects' behavior='link' />
+            <Button text='Projects' type='ghost' radius='md' href='/#projects' behavior='link' />
           </li>
           <li>
-            <Button text='Stack' type='ghost' radius='md' href='#stacks' behavior='link' />
+            <Button text='Stack' type='ghost' radius='md' href='/#stacks' behavior='link' />
           </li>
           <li>
             <Button text='Resume' type='ghost' radius='md' href='' behavior='link' />
           </li>
           <li>
-            <Button text='Contact' type='ghost' radius='md' href='#contact' behavior='link' />
+            <Button text='Contact' type='ghost' radius='md' href='/#contact' behavior='link' />
           </li>
         </ul>
       </nav>
