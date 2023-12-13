f87fda2f04e290684d368a567ae207e8e18c1fe6
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-13 11:52:55 +0530

    add analytics

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -41,6 +41,15 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRender
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700;800&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-00ZDLV4JQ0"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-00ZDLV4JQ0');
+        </script>
         <title>${title}</title>
       </head>
       <body>
