50b83bb9571d4fab5c8b1982ee8bf0717bcb49b6
afkcodes <ashish.kmr@outlook.com>
2025-08-30 13:16:20 +0530

    Remove files that don't exist in v2 branch
    - Cleaned up old project structure files
    - Removed Vike/SSR framework files
    - Removed TailwindCSS and related configs
    - Dev branch now has identical file structure to v2

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: 'none',
-  singleQuote: true,
-  printWidth: 100,
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  jsxSingleQuote: true
-};
--- pages/index/+config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  title: `Ashish's Personal Space | afk.codes`,
-  description:
-    "Hello there! I'm Ashish a developer, an explorer, and a music enthusiast. I enjoy experimenting with web technologies."
-};
